b795827753d44da28d090e40fa4efd8e5cbb8f4a
kimlangui <kim.langui@gmail.com>
2021-06-12 18:03:24 +0200

    create dolphins/cats kindof works

--- ui/js/script.js
+++ ui/js/script.js
@@ -1,10 +1,27 @@
-function addData(link, elements) {
+let cat_link = 'http://localhost:8000/cats/';
+let cat_param = ["name", "characteristics"];
+var read_cats = () => read(cat_link, cat_param);
+var create_cat = () => create(cat_link, cat_param);
+
+let dog_link = 'http://localhost:8080/dogs/';
+let dog_param = ["name", "breed"];
+var read_dogs = () => read(dog_link, dog_param);
+var create_dog = () => create(dog_link, dog_param);
+
+let dolphin_link = 'http://localhost:8001/dolphins/';
+let dolphin_param = ["name", "color", "age"];
+var read_dolphins = () => read(dolphin_link, dolphin_param);
+var create_dolphin = () => create(dolphin_link, dolphin_param);
+
+var capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1)
+
+function read(link, elements) {
     var mainContainer = document.getElementsByTagName("table")[0];
 
     var tr = document.createElement("tr");
     elements.forEach(elem => {
         var h = document.createElement("th");
-        h.innerHTML = elem.charAt(0).toUpperCase() + elem.slice(1);
+        h.innerHTML = capitalize(elem);
         tr.append(h);
     });
     var h = document.createElement("th");
@@ -35,15 +52,10 @@ function addData(link, elements) {
                 adopt.id = data[i].id;
                 adopt.onclick = function remove() {
                     $.ajax({
-                        // url: 'http://localhost:7000/dolphins/'+ i +'/',
-                        // url: 'https://localhost:8243/dolphins/1/dolphins/'+ i +'/',
                         url: link + this.id +'/',
                         type: 'DELETE',
                         contentType: 'application/json',
                         dataType: 'text',
-                        /* beforeSend: function(xhr) {
-                            xhr.setRequestHeader("apikey", "eyJ4NXQiOiJOVGRtWmpNNFpEazNOalkwWXpjNU1tWm1PRGd3TVRFM01XWXdOREU1TVdSbFpEZzROemM0WkE9PSIsImtpZCI6ImdhdGV3YXlfY2VydGlmaWNhdGVfYWxpYXMiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhZG1pbkBjYXJib24uc3VwZXIiLCJhcHBsaWNhdGlvbiI6eyJvd25lciI6ImFkbWluIiwidGllclF1b3RhVHlwZSI6bnVsbCwidGllciI6IlVubGltaXRlZCIsIm5hbWUiOiJEZWZhdWx0QXBwbGljYXRpb24iLCJpZCI6MSwidXVpZCI6ImUxNjI2OTljLWUxYzMtNGQyMy05OGUxLTE0YmIxYjViMzhlNCJ9LCJpc3MiOiJodHRwczpcL1wvbG9jYWxob3N0Ojk0NDNcL29hdXRoMlwvdG9rZW4iLCJ0aWVySW5mbyI6eyJCcm9uemUiOnsidGllclF1b3RhVHlwZSI6InJlcXVlc3RDb3VudCIsImdyYXBoUUxNYXhDb21wbGV4aXR5IjowLCJncmFwaFFMTWF4RGVwdGgiOjAsInN0b3BPblF1b3RhUmVhY2giOnRydWUsInNwaWtlQXJyZXN0TGltaXQiOjAsInNwaWtlQXJyZXN0VW5pdCI6bnVsbH19LCJrZXl0eXBlIjoiU0FOREJPWCIsInBlcm1pdHRlZFJlZmVyZXIiOiIiLCJzdWJzY3JpYmVkQVBJcyI6W3sic3Vic2NyaWJlclRlbmFudERvbWFpbiI6ImNhcmJvbi5zdXBlciIsIm5hbWUiOiJEb2xwaGlucyIsImNvbnRleHQiOiJcL2RvbHBoaW5zXC8xIiwicHVibGlzaGVyIjoiYWRtaW4iLCJ2ZXJzaW9uIjoiMSIsInN1YnNjcmlwdGlvblRpZXIiOiJCcm9uemUifSx7InN1YnNjcmliZXJUZW5hbnREb21haW4iOiJjYXJib24uc3VwZXIiLCJuYW1lIjoiQ2F0cyIsImNvbnRleHQiOiJcL2NhdHNcLzEuMCIsInB1Ymxpc2hlciI6ImFkbWluIiwidmVyc2lvbiI6IjEuMCIsInN1YnNjcmlwdGlvblRpZXIiOiJCcm9uemUifSx7InN1YnNjcmliZXJUZW5hbnREb21haW4iOiJjYXJib24uc3VwZXIiLCJuYW1lIjoiRG9ncyIsImNvbnRleHQiOiJcL2RvZ3NcLzEuMCIsInB1Ymxpc2hlciI6ImFkbWluIiwidmVyc2lvbiI6IjEuMCIsInN1YnNjcmlwdGlvblRpZXIiOiJCcm9uemUifSx7InN1YnNjcmliZXJUZW5hbnREb21haW4iOiJjYXJib24uc3VwZXIiLCJuYW1lIjoiRG9scGhpbnMiLCJjb250ZXh0IjoiXC9kb2xwaGluc1wvMS4xIiwicHVibGlzaGVyIjoiYWRtaW4iLCJ2ZXJzaW9uIjoiMS4xIiwic3Vic2NyaXB0aW9uVGllciI6IkJyb256ZSJ9XSwicGVybWl0dGVkSVAiOiIiLCJpYXQiOjE2MjMzNDcwNzYsImp0aSI6ImNhMzRhNDRmLWFhNGYtNDc5NC1iNzZjLTQ4MGU4ODk5YmE3NyJ9.qXPa1D_vRAlEKeCqgsmCAgqY7hblk4XKJU9eofFfmlIkmKhkqFwvzJu5kOC28WulSbLvcruiG_D1M_gpZQZhTXfOQ2cENklvKHimEOBGYZ9X6qpj7d91_Gme_1p01ir7ufJX-EZjUNh_geRAWRcGFBm6HqTAB6o7MNU0V5LW2AVYmwlDD6T55yCbvE2IEq8lo7w95LB-eNMSyFWQD5IQHGTXfjDT3fCMZxR49bYPhxOyZj2TGdcPefrCTJQy9Jp0Yzohk8dZ-vJTe3s0qvy4q1lYOb18zjBQDhSHGuZpp0V8wb_WrCYgxPJS4iIPwZ-x4Wtt0az4T-9wzeUvHVHZHA==")
-                        }, */
                         success: function r() { location.reload() }
                     });
                 }
@@ -70,4 +82,47 @@ function addData(link, elements) {
     }
     )
     mainContainer.append(body)
+}
+
+function create(link, elements) {
+    var mainContainer = document.getElementsByTagName("form")[0];
+    mainContainer.action = link;
+    mainContainer.method = "POST";
+    elements.forEach(elem => {
+        var label = document.createElement("label");
+        label.classList.add("form-label");
+        label.htmlFor = elem;
+        label.innerHTML = capitalize(elem);
+
+        var input = document.createElement("input");
+        input.classList.add("form-text");
+        input.name = elem;
+        input.id = elem;
+        input.type = "text";
+
+        var p = document.createElement("p");
+        p.append(label);
+        p.append(input);
+        mainContainer.append(p);
+    });
+
+    var input = document.createElement("input");
+    input.classList.add("btn-secondary");
+    input.value = "I'll miss you...";
+    input.id = "submit";
+    input.type = "submit";
+    input.onclick = function giveUp() {
+        $.ajax({
+            type: "POST",
+            url: link,
+            data: JSON.stringify(mainContainer.serializeArray()),
+            dataType: "json",
+            contentType : "application/json",
+            success: window.location.href = "read.html"
+          });
+    };
+
+    var p = document.createElement("p");
+    p.append(input);
+    mainContainer.append(p);
 }
